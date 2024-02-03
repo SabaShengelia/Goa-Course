b3261193fae0125db5d22ddac88a5fa99f9c4126
SabaShengelia <sabashengelia24@gmail.com>
2024-02-03 17:47:42 +0400

    first commit

---
+++ day 00/homework/lesson 0.py
@@ -0,0 +1,107 @@
+# from turtle import *
+
+# #i want to create a house
+
+# #step 1: draw a square
+# speed(30)
+# width(7)
+# color("purple")
+# forward  (200)
+# left(90)
+
+# forward(200)
+# left(90)
+
+# forward(200)
+# left(90)
+
+# forward(200)
+# left(90)
+
+# #end of square
+
+# #draw a door
+
+# forward(70)
+# color("yellow")
+# begin_fill()
+# left(90)
+
+# forward(120)
+# right(90)
+
+# forward(60)
+# right(90)
+
+# forward(120)
+# end_fill()
+
+# penup()
+# goto(200, 200)
+# pendown()
+
+# color("red")
+# begin_fill()
+# right(150)
+# forward(200)
+# left(120)
+# forward(200)
+# end_fill()
+
+
+
+
+
+
+
+# exitonclick()
+
+
+
+from turtle import *
+# tree drawing start
+
+begin_fill()
+color ("brown")
+width (6)
+forward(40)
+left (90)
+forward(100)
+left(90)
+
+forward(40)
+left(90)
+forward(100)
+end_fill()
+
+
+# leafs drawing start
+
+penup()
+goto (40, 100)
+pendown()
+begin_fill()
+color("green")
+left(90)
+forward(30)
+left(90)
+forward(90)
+left(90)
+forward(100)
+left(90)
+forward(90)
+left(90)
+forward(70)
+end_fill()
+
+#end drawing of leafs
+
+
+
+
+
+
+
+
+
+exitonclick()
