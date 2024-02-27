ffc8721c236995202cfd346ef49b0365988b4e75
SabaShengelia <sabashengelia24@gmail.com>
2024-02-28 01:03:12 +0400

    fifth commit

---
+++ day 08/classwork/classwork8.py
@@ -0,0 +1,10 @@
+required_apple = 10
+required_banana = 5
+
+apple= (input("How Many Apple Did You Ate:"))     
+banana = (input("How Many Banana Did You Ate:"))
+
+print(required_apple and required_banana)
+
+
+
