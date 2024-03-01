f5bcb7c4583e82a7ff38081901949ce0effc46ea
SabaShengelia <sabashengelia24@gmail.com>
2024-03-01 23:06:39 +0400

    first commit

--- day 06/homework/lesson6.py
+++
@@ -1,9 +0,0 @@
-firstname = input("Please Enter Your Firstname: ")
-
-lastname = input("Please Enter Your Lastname: ")
-
-age = input("Please Enter Your Age: ")
-
-password = input("Please Enter Password: ")
-
-print( "Your Account Succesfully Created: " ,firstname + " " + lastname + " " +  age + " " + password)
