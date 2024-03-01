c0b59ef107e0fe24bdaedc3bff65b833001fd688
SabaShengelia <sabashengelia24@gmail.com>
2024-03-01 23:13:52 +0400

    tenth commit

---
+++ day 09/classwork/lesson9.py
@@ -0,0 +1,29 @@
+# i = 1
+
+# while i <= 100:
+#     print(i)
+#     i = i + 1
+
+# i = 100
+
+# while i == 0:
+#     print(i)
+#     i = i - 1
+
+
+# i = 0
+# sum = 10
+
+# while i <= 10:
+#     print(i)
+#     sum = sum + i
+#     i = i + 2
+
+# print(sum)
+    
+
+# i = 1
+
+# while i < 10:
+#     print(i)
+#     i = i + 2
