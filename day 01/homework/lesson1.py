ac509596c9670da81b4c0f8f1842666a8d705948
SabaShengelia <sabashengelia24@gmail.com>
2024-03-01 23:07:30 +0400

    second commit

---
+++ day 01/homework/lesson1.py
@@ -0,0 +1,6 @@
+name="i am saba"
+username="shengelia"
+age="18 years old"
+story="i entered goal oriented academy because i always wanted to learn programming"
+
+print (name + " " + str(username) + " " + str(age) + " " + str(story))
