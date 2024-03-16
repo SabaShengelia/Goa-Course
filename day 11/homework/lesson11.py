949396ddf85de7a88dce2869853459c5c9891fa2
SabaShengelia <sabashengelia24@gmail.com>
2024-03-16 23:24:42 +0400

    thirteenth commit

--- day 11/homework/lesson11.py
+++ day 11/homework/lesson11.py
@@ -1,120 +1,120 @@
 #First Homework
 
-# i = 0
+i = 0
 
-# while i <= 10:
-#     print(i)
-#     i = i + 1
+while i <= 10:
+    print(i)
+    i = i + 1
 
-# if i:
-#     print("Time's Up")
+if i:
+    print("Time's Up")
 
-# else:
-#     print("Time's Still Counting")
+else:
+    print("Time's Still Counting")
 
 #First Homework
 
 #Second Homework
 
-# sum = 0
+sum = 0
 
-# num = int(input("Please Enter Number: "))
+num = int(input("Please Enter Number: "))
 
 
-# while num != 0:
+while num != 0:
 
-#     num = int(input("Please Enter Number: "))
+    num = int(input("Please Enter Number: "))
     
-#     sum = sum + num
+    sum = sum + num
 
-# print(sum)
+print(sum)
 
 #Second Homework
 
 #Third Homework
 
-# num = int(input("Please Enter password: "))
-# unit_password  = 12345
+num = int(input("Please Enter password: "))
+unit_password  = 12345
 
-# while num != unit_password:
-#     num = int(input("Please Enter password: "))
+while num != unit_password:
+    num = int(input("Please Enter password: "))
     
-#     if num == unit_password:
-#         print("Correct Password! ")
+    if num == unit_password:
+        print("Correct Password! ")
     
-#     else:
-#         print("Incorrect Password! ")
+    else:
+        print("Incorrect Password! ")
 
 #Third Homework
 
 #Fourth Homework
 
-# i = 0
+i = 0
 
-# while i <= 20:
-#     print(i)
-#     i = i + 2    
+while i <= 20:
+    print(i)
+    i = i + 2    
     
 #Fourth Homework
 
 #Fiveth Homework
 
-# sum  = 0
+sum  = 0
 
-# num = int(input("Please Enter Number: "))
+num = int(input("Please Enter Number: "))
 
-# while num >= 0:
-#     num = int(input("Please Enter Number: "))
+while num >= 0:
+    num = int(input("Please Enter Number: "))
 
-#     if num >= 0:
-#         print("Try Again")
-#     else:
-#         print("Correct")
-#     sum = sum + num
+    if num >= 0:
+        print("Try Again")
+    else:
+        print("Correct")
+    sum = sum + num
 
-# print(sum)
+print(sum)
     
 
 #Fiveth Homework
 
 #Sixth Homework
 
-# num = int(input("Please Enter Number: "))
+num = int(input("Please Enter Number: "))
 
-# if num % 2:
-#     print("Number Is Even")
-# else:
-#     print("Number Is Odd")
+if num % 2:
+    print("Number Is Even")
+else:
+    print("Number Is Odd")
 
 #Sixth Homework
         
 #Seventh Homework
 
-# num1 = int(input("Please Enter Temperature: "))
+num1 = int(input("Please Enter Temperature: "))
 
-# if num1 > 30:
-#     print("High Temperature")
-# elif num1 > 20 and num1 < 30:
-#     print("Medium Tempereture")
-# else:
-#     print("Low Temperature")
+if num1 > 30:
+    print("High Temperature")
+elif num1 > 20 and num1 < 30:
+    print("Medium Tempereture")
+else:
+    print("Low Temperature")
 
 #Seventh Homework
 
 #Eighth Homework
 
-# score = int(input("Please Enter Your Exam Score: "))
+score = int(input("Please Enter Your Exam Score: "))
 
-# if score > 90 and score < 100:
-#     print("Congrats You Got A")
-# elif score > 80 and score < 89:
-#     print("Congrats You Got B")
-# elif score > 70 and score < 79:
-#     print("Congrats You Got C")
-# elif score > 60 and score < 69:
-#     print("Congrats You Got D")
-# else:
-#     print("You Got F")
+if score > 90 and score < 100:
+    print("Congrats You Got A")
+elif score > 80 and score < 89:
+    print("Congrats You Got B")
+elif score > 70 and score < 79:
+    print("Congrats You Got C")
+elif score > 60 and score < 69:
+    print("Congrats You Got D")
+else:
+    print("You Got F")
 
 #Eighth Homework
 
@@ -124,47 +124,45 @@
 
 #Tenth Homework
 
-# num = int(input("Please enter first number: "))
-# num1 = int(input("Please enter second number: "))
+num = int(input("Please enter first number: "))
+num1 = int(input("Please enter second number: "))
 
-# if num > num1:
-#     print("number1 are more than number2")
-# else:
-#     print("number1 are less than number2 ")
+if num > num1:
+    print("number1 are more than number2")
+else:
+    print("number1 are less than number2 ")
 
 #Tenth Homework
 
 #Eleventh Homework
 
-# number = 21
+number = 21
 
-# num = int(input("Please enther number: "))
+num = int(input("Please enther number: "))
 
-# while num != number:
-#     num = int(input("Please enther number: "))
+while num != number:
+    num = int(input("Please enther number: "))
 
-# if num == number:
-#     print("You Guessed Correct")
-# else:
-#     ("Try Again")
+if num == number:
+    print("You Guessed Correct")
 
 #Eleventh Homework
 
-# num = int(input("Please enter number: "))
+num = int(input("Please enter number: "))
 
-# while num % 2:
-#     num = int(input("Please enter number: "))
+while num % 2:
+    num = int(input("Please enter number: "))
 
-# if num % 3:
-#     print("Correct! ")
-# else:
-#     print("Incorrect! ")
+if num % 3:
+    print("Correct! ")
+else:
+    print("Incorrect! ")
 
 #Eleventh Homework
 
-#Twelveth Homework
+#Twelfth Homework
 
-#Twelveth Homework
+#Twelfth Homework
 
 #Threeteenth Homework
 
