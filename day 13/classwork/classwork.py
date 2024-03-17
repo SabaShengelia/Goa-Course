bd276d913a47d06044f7d7c910d853d1df23a50a
SabaShengelia <sabashengelia24@gmail.com>
2024-03-17 19:51:14 +0400

    fourteen commit

---
+++ day 13/classwork/classwork.py
@@ -0,0 +1,29 @@
+
+
+# age = (int(input("Please enter your age: ")))
+
+# if age < 18:
+#     print("You are kid")
+# elif age > 18 and age < 50:
+#     print("You are adult")
+# else:
+#     print("You are old man")
+
+
+
+
+
+num = int(input("Please Enter Number: "))
+
+while num >= 0:
+    num = int(input("Please Enter Number: "))
+
+    if num >= 0:
+        print("Try Again")
+    else:
+        print("Correct")
+    
+
+
+
+
---
+++ day 13/homework/lesson13.py
@@ -0,0 +1,124 @@
+#First Homework
+
+age = int(input("Please enter your age: "))
+
+if age > 50:
+    print("You are old")
+elif age <= 50 and age >= 18:
+    print("you are adult")
+else:
+    print("you are kid")
+
+#First Homework
+    
+#Second Homework
+    
+num = int(input("Please enter number: "))
+
+if num % 2:
+    print("Number Is Odd")
+else:
+    print("Number Is Even")
+
+num1 = int(input("Please enter first number: "))
+num2 = int(input("Please enter second number: "))
+num3 = int(input("Please enter third number: "))
+num4 = int(input("Please enter fourth number: "))
+num5 = int(input("Please enter fiveth number: "))
+
+if num1 % 2:
+    print("Number Is Odd")
+else:
+    print("Number Is Even")
+if num2 % 2:
+    print("Number Is Odd")
+else:
+    print("Number Is Even")
+if num3 % 2:
+    print("Number Is Odd")
+else:
+    print("Number Is Even")
+if num4 % 2:
+    print("Number Is Odd")
+else:
+    print("Number Is Even")
+if num5 % 2:
+    print("Number Is Odd")
+else:
+    print("Number Is Even")
+
+#Second Homework
+
+#Third Homework
+
+
+score = input("Please enter your exam score: ")
+
+A = "Exellent"
+B = "Good Job"
+C = "You Passed"
+D = "You Can Do Better"
+F = "You Failed"
+
+if score == "A":
+    print(A)
+elif score == "B":
+    print(B)
+elif score == "C":
+    print(C)
+elif score == "D":
+    print(D)
+elif score == "F":
+    print(F)
+
+#Third Homework
+
+#Fourth Homework
+
+i = 0
+
+while i < 10 + 1:
+    print(i)
+    i = i + 1
+
+#Fourth Homework
+
+#Fiveth Homework
+
+num = int(input("Please guess the number between 0, 10: "))
+
+while num != 7:
+    num = int(input("Please guess the number between 0, 10: "))
+    if num == 7:
+        print("Correct! ")
+    else:
+        print("Incorrect! ")
+
+#Fiveth Homework
+
+#Sixth Homework
+
+for i in range(5, 15 + 1,):
+    print(i)
+
+#Sixth Homework
+
+#Seventh Homework
+
+for i in range(10, -1, - 1, ):
+    print(i)
+    
+#Seventh Homework
+    
+
+
+
+
+
+
+
+
+
+
+
+
