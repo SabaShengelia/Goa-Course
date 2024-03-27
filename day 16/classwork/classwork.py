b871752c33248daf5f3fa18d9750d9eb9da397aa
SabaShengelia <sabashengelia24@gmail.com>
2024-03-27 17:46:28 +0400

    seventeenth commit

---
+++ day 16/classwork/classwork.py
@@ -0,0 +1,60 @@
+sports = [
+    "soccer",
+    "basketball",
+    "baseball",
+    "football",
+    "valleyball"
+]
+
+sodas = [
+    "pepsi",
+    "cocacola",
+    "sprite",
+    "fanta"
+]
+
+print(sports)
+print(sports[2])
+print(sodas[3])
+
+name = "saba"
+lastname = "shengelia"
+
+print(name[0], "." ,lastname[0])
+
+soda = [
+    "sprite",
+    "cocacola",
+    "pepsi",
+    "fanta"
+]
+
+sport = [
+    "football",
+    "soccer",
+    "basketball",
+    "baseball"
+]
+
+print(len(soda + sport))
+
+surname = "shengelia"
+
+print(surname[8])
+
+name = [
+    "saba",
+    "nika",
+    "dato",
+    "luka"
+]
+
+surname = [
+    "shengelia",
+    "baxtadze",
+    "nikuradze",
+    "shanidze"
+]
+
+print(name + surname)
+
---
+++ day 16/homework/lesson16.py
@@ -0,0 +1,83 @@
+#First Homework
+
+movie = [
+    "Insidious",
+    "After Earth",
+    "Eli's Book",
+    "World War Z",
+    "I Am Legend"
+]
+
+print(movie)
+print(movie[0])
+
+#First Homework
+
+#Second Homework
+
+name = [
+    "giorgi",
+    "saba",
+    "luka",
+    "nika",
+    "dato"
+]
+
+brand = [
+    "nike",
+    "adidas",
+    "puma",
+    "gucci",
+    "prada"
+]
+
+print(name + brand)
+
+#Second Homework
+
+#Third Homework
+
+name = "saba"
+
+print(name[0] + name[3])
+
+#Third Homework
+
+#Fourth Homework
+
+programming_language = [
+    "python",
+    "javascript",
+    "css",
+    "java"
+]
+
+print(len(programming_language))
+
+#Fourth Homework
+
+#Fiveth Homework
+
+fruit = [
+    "apple",
+    "banana",
+    "orange",
+    "strawberry"
+]
+
+fruit[3] = "tangerine"
+
+print(fruit)
+
+#Fiveth Homework
+
+#Sixth Homework
+
+nums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+
+nums_sum = [1] + [10]
+
+print(nums)
+print(sum(nums_sum))
+
+#Sixth Homework
